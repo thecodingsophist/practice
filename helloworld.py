5ecdc880efbbe14754c97b5eed9296d30e75b9e3
Zhanxi Ni <ni.zhanxi@gmail.com>
2021-05-26 19:44:44 -0400

    easy python leet

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,11 @@
+#Exercises
+
+# print Hello World
 print("hello world")
+
+# self dividing numbers : numbers in which the digits in the number can divide the original number, self dividing number cannot contain a 0 digit, and we are given the left and right boundaries, inclusive
+
+# : thoughts : we have to go through every number, so runtime is n, at least
+
+def selfDividingNumber(a, b):
+
